8f093d349d3f4e0335fd14d2908baec0fad60be9
Tristan <119224484+Papilegend@users.noreply.github.com>
2023-12-20 11:57:27 +0200

    Add files via upload

---
+++ graafid.py
@@ -0,0 +1,5 @@
+naidis_sonastik = {
+
+    "London": [["Helsingi", 40], ["Tallinn", 50]],
+    "New York": [["Sydney", 2000], ["Tokyo", 5000]]
+}
---
+++ main.py
@@ -0,0 +1,11 @@
+from graafid import list_2
+
+print(list_2)
+
+
+
+
+
+
+
+
