1378a08dacb4759a2bb33588c11edcdbb9e256d0
Tristan <119224484+Papilegend@users.noreply.github.com>
2023-12-21 10:50:56 +0200

    Update graafid.py

--- graafid.py
+++ graafid.py
@@ -1,5 +1,8 @@
-naidis_sonastik = {
+cities_distances = {
 
-    "London": [["Helsingi", 40], ["Tallinn", 50]],
-    "New York": [["Sydney", 2000], ["Tokyo", 5000]]
+    "Tallinn": [["London", 1800], ["Helsingi", 90], ["Madrid", 2800], ["Pariis", 1800], ["Istanbul", 2000]],
+    "Helsingi": [["London", 1820], ["Tallinn", 90], ["New York", 6500], ["Kairo", 7500]],
+    "New York": [["London", 5500], ["Sydney", 15000], ["Tokyo", 11000], ["Helsingi", 6500]],
+    
+
 }
